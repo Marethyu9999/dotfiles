5fad552428515ddc1066fe6b16386bd0b6a97ba6
Erik <erik@grobecker.me>
2023-05-30 21:53:11 +0200

    added dwm to system config

--- configuration.nix
+++ configuration.nix
@@ -76,6 +76,7 @@ in {
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
   services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.windowManager.dwm.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
