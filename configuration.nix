1506d3148d6b88c2985971836e4cdf95fc56c071
Erik <erik@grobecker.me>
2023-06-03 00:42:04 +0200

    misswrote somethign

--- configuration.nix
+++ configuration.nix
@@ -31,7 +31,7 @@ in {
   # Enable swap on luks
   boot.initrd.luks.devices."luks-8d73b112-3c31-48a2-8697-335a26d94081" = {
       gpgCard.publicKey = "/home/marethyu/git/sys-config/publickey.asc";
-      gpgcard = true;
+      gpgCard = true;
     };
   boot.initrd.luks.devices."luks-8d73b112-3c31-48a2-8697-335a26d94081".device = "/dev/disk/by-uuid/8d73b112-3c31-48a2-8697-335a26d94081";
   boot.initrd.luks.devices."luks-8d73b112-3c31-48a2-8697-335a26d94081".keyFile = "/crypto_keyfile.bin";
