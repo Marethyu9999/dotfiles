4a04c264d615c995eaa7abbffd5d386623ea1813
Erik <erik@grobecker.me>
2023-06-04 23:39:36 +0200

    moved both to / again
    the builtin function can't read the git path it seems

--- configuration.nix
+++ configuration.nix
@@ -29,8 +29,8 @@ in {
   # };
   boot.initrd.luks.gpgSupport = true;
   boot.initrd.luks.devices."luks-9867a9d4-5c3b-44e9-bd61-2e0124e11436".gpgCard = {
-      encryptedPass = builtins.readFile "/home/marethyu/git/sys-config/luks-password.txt.asc";
-      publicKey = builtins.readFile "/home/marethyu/git/sys-config/publickey.asc";
+      encryptedPass = builtins.readFile "/luks-password.txt.asc";
+      publicKey = builtins.readFile "/publickey.asc";
     };
 
   # Enable swap on luks
