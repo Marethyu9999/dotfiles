ca313cee87b7499b7674218f85f93b1baba683ee
Erik <erik@grobecker.me>
2023-08-28 19:23:32 +0200

    Revert "configuration.nix: added touchegg"
    This reverts commit 54250e5d34c7371460bfadd540aa5942fdcae342.

--- configuration.nix
+++ configuration.nix
@@ -67,7 +67,6 @@ in {
   fonts.packages = [ pkgs.font-awesome ];
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.touchegg.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
