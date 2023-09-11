f280c75b52d32693fa2f305d60aaa3ed3762fc72
Erik <erik@grobecker.me>
2023-09-11 22:34:26 +0200

    chezmoi: updated doom config to new folder

--- chezmoi/dot_config/doom/custom.el
+++ chezmoi/dot_config/doom/custom.el
@@ -3,8 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil)
- '(warning-suppress-types '((doom-first-file-hook) (defvaralias))))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(newsticker-url-list
+   '(("Der Postellion" "https://follow.it/der-postillon-abo" nil nil nil)))
+ '(package-selected-packages '(xah-fly-keys)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
